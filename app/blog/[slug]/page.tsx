291374361a82497b4960704d14775d0a9ba37ad6
BenHM10 <88811452+BenHM10@users.noreply.github.com>
2026-04-27 16:37:25 +0700

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,12 @@
-export default function BlogDetail({ params }) {
-    return (
-      <div className="p-10">
-        <h1 className="text-2xl font-bold">Promo: {params.slug}</h1>
-        <p className="mt-4">Check out our latest internet promotions.</p>
-      </div>
-    );
-  }
+export default function BlogDetail({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Promo: {params.slug}</h1>
+      <p className="mt-4">Check out our latest internet promotions.</p>
+    </div>
+  );
+}
